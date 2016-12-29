19c582e361cb697effa2d3e993e8709d61d3e640
crashdp <crashdp@crashdp>
2016-12-29 20:18:41 +0200

    index.php add log in form

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ session_start();
     </head>
     <body>
         <?php if (empty($_SESSION['login'])): ?>
+        <form method="post">
+            <label>
+                <input type="text" name="login"/>
+            </label>
+            <input type="submit" value="log in"/>
+        </form>
         <?php else: ?>
         <?php endif; ?>
     </body>
